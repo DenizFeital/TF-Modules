89b1a4bfd62fe61fbd0f8662514097191acde728
Deniz Feital Armanhe <49575782+DenizFeital@users.noreply.github.com>
2024-07-24 10:00:28 -0300

    interface

--- virtual_machine/main.tf
+++ virtual_machine/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_windows_virtual_machine" "main" {
   admin_password        = "${mypassword}"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
+  network_interface_ids = myterraform-nic.id
   size                  = "Standard_DS1_v2"
 
   os_disk {
