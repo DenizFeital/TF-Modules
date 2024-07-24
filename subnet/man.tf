36c00de87779c1f1d1a7262d4236b54cec9808c5
Deniz Feital Armanhe <49575782+DenizFeital@users.noreply.github.com>
2024-07-24 10:12:34 -0300

    new location

--- subnet/man.tf
+++ subnet/man.tf
@@ -2,29 +2,29 @@
 resource "azurerm_virtual_network" "my_terraform_network" {
   name                = "myterrafor-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = resource_group_location
-  resource_group_name = resource_group_name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
 }
 
 # Create subnet
 resource "azurerm_subnet" "my_terraform_subnet" {
   name                 = "subnet-sub"
-  resource_group_name  = resource_group_name
+  resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.my_terraform_network.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 # Create public IPs
 resource "azurerm_public_ip" "my_terraform_public_ip" {
   name                = "pub-ip"
-  location            = resource_group_location 
-  resource_group_name = resource_group_name
+  location            = var.resource_group_location 
+  resource_group_name = var.resource_group_name
   allocation_method   = "Dynamic"
 }
 # Create Network Security Group and rules
 resource "azurerm_network_security_group" "my_terraform_nsg" {
   name                = prefix+"-nsg"
-  location            = resource_group_location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
 
   security_rule {
     name                       = "RDP"
--- subnet/variables.tf
+++ subnet/variables.tf
@@ -7,6 +7,11 @@ variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
 }
+variable "resource_group_location" {
+  description = "The name of the resource group"
+  type        = string
+}
+
 
 variable "virtual_network_name" {
   description = "The name of the virtual network"
