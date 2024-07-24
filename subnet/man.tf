e252cc55fd4aaa72d01b195ac584bb9d0066f80a
Deniz Feital Armanhe <49575782+DenizFeital@users.noreply.github.com>
2024-07-24 10:02:59 -0300

    ids

--- subnet/man.tf
+++ subnet/man.tf
@@ -8,14 +8,14 @@ resource "azurerm_virtual_network" "my_terraform_network" {
 
 # Create subnet
 resource "azurerm_subnet" "my_terraform_subnet" {
-  name                 = prefix+"-sub"
+  name                 = "subnet-sub"
   resource_group_name  = resource_group_name
   virtual_network_name = azurerm_virtual_network.my_terraform_network.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 # Create public IPs
 resource "azurerm_public_ip" "my_terraform_public_ip" {
-  name                = prefix+"-pub-ip"
+  name                = "pub-ip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = resource_group_name
   allocation_method   = "Dynamic"
@@ -45,7 +45,7 @@ resource "azurerm_network_interface" "my_terraform_nic" {
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = prefix+"-conf"
+    name                          = "dfa-ip-conf"
     subnet_id                     = azurerm_subnet.my_terraform_subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
