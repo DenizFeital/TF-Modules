57ca2c8221b4ebdc8fb33e9e2a82bec42cd4bfc7
Deniz Feital Armanhe <49575782+DenizFeital@users.noreply.github.com>
2024-07-25 07:52:57 -0300

    new day

--- virtual_machine/main.tf
+++ virtual_machine/main.tf
@@ -2,7 +2,7 @@
 resource "azurerm_windows_virtual_machine" "main" {
   name                  = "dfa-vm"
   admin_username        = var.admin_username
-  admin_password        = var.mypassword
+  admin_password        = {var.mypassword}
   location              = var.resource_group_location
   resource_group_name   = var.resource_group_name
   network_interface_ids = var.nic_name.id
