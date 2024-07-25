704b61f5c4f5d9f287d7ba785d43016d014c4004
Deniz Feital Armanhe <49575782+DenizFeital@users.noreply.github.com>
2024-07-25 07:57:55 -0300

    new one

--- virtual_machine/main.tf
+++ virtual_machine/main.tf
@@ -1,18 +1,21 @@
 # Create virtual machine
+#added again
 resource "azurerm_windows_virtual_machine" "main" {
-  name                  = "dfa-vm"
+  name                  = "DFA-vm"
   admin_username        = var.admin_username
-  admin_password        = {var.mypassword}
-  location              = var.resource_group_location
-  resource_group_name   = var.resource_group_name
-  network_interface_ids = var.nic_name.id
+  admin_password        = "Password@1650"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
   size                  = "Standard_DS1_v2"
 
   os_disk {
-    name                 = "dfa-os-Disk"
+    name                 = "DFA-Disk"
     caching              = "ReadWrite"
     storage_account_type = "Premium_LRS"
   }
+
+
 
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
