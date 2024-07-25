1259ccc93b85e057c2a56b9a53fe088f9544bf49
Deniz Feital Armanhe <49575782+DenizFeital@users.noreply.github.com>
2024-07-25 08:15:29 -0300

    6

--- subnet/main.tf
+++ subnet/main.tf
@@ -2,8 +2,8 @@
 resource "azurerm_virtual_network" "my_terraform_network" {
   name                = "myterrafor-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = var.resource_group_name
-  resource_group_name = var.resource_group_location
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
 }
 
 # Create subnet
