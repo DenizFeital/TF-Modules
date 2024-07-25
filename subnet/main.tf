5c16bcf6d2612428344895bf240bc03d201f3c19
Deniz Feital Armanhe <49575782+DenizFeital@users.noreply.github.com>
2024-07-25 08:49:24 -0300

    sub

--- subnet/main.tf
+++ subnet/main.tf
@@ -45,7 +45,7 @@ resource "azurerm_network_interface" "main" {
   resource_group_name = var.resource_group_name
   ip_configuration {
     name                          = "dfa-ip-conf"
-    subnet_id                     = "1"
+    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
     }
