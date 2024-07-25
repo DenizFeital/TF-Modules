9a44182294f86da1143ca02f36ae15c219efbcaf
Deniz Feital Armanhe <49575782+DenizFeital@users.noreply.github.com>
2024-07-25 08:39:01 -0300

    subnetid

--- resource_group/variables.tf
+++ resource_group/variables.tf
@@ -7,3 +7,7 @@ variable "location" {
   description = "The location of the resource group"
   type        = string
 }
+variable "subnet_id" {
+  description = "The ID of the subnet to which the network interface will be connected."
+  type        = string
+}
