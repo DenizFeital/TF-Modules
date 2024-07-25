4f38be51bd8f47d6a6ff66727ea539c73f070aa2
Deniz Feital Armanhe <49575782+DenizFeital@users.noreply.github.com>
2024-07-25 08:11:41 -0300

    5

--- virtual_machine/main.tf
+++ virtual_machine/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_windows_virtual_machine" "main" {
   admin_password        = "Password@1650"
   location              = "East-US"
   resource_group_name   = var.resource_group_name
-  network_interface_ids = "1"
+  network_interface_ids = azurerm_windows_virtual_machine.main.ID
   size                  = "Standard_DS1_v2"
 
   os_disk {
